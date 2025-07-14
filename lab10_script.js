128b1405f16e2b5eb586beaeff2731e627ac1983
fernando22-alt <fernandoluisramirezsierra2@gmail.com>
2025-07-14 18:02:25 -0500

    Add files via upload

---
+++ lab10_script.js
@@ -0,0 +1,18 @@
+let nombre = "Fernando";
+let edad = 22;
+let esEstudiante = true;
+console.log(nombre, edad, esEstudiante);
+
+let a = 10, b = 5;
+console.log("Suma:", a + b);
+console.log("Resta:", a - b);
+console.log("Multiplicación:", a * b);
+console.log("División:", a / b);
+console.log("Comparación:", a > b, a == b, a != b);
+
+let esMayorDeEdad = true;
+let tieneLicencia = false;
+console.log("¿Puede conducir?:", esMayorDeEdad && tieneLicencia);
+
+let mensaje = `Hola, ${nombre}. Tienes ${edad} años.`;
+console.log(mensaje);
